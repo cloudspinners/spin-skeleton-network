ed1432a5ff474c316704b529b9604760ca2edd10
kief <kmorris@thoughtworks.com>
2018-07-16 15:46:42 +0100

    Basic network implementation

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source 'https://rubygems.org'
 
 gem 'rake'
 gem 'rake_cloudspin'
+# gem 'rake_cloudspin', :path => '../rake_cloudspin'
 gem 'aws_ssh_key'
 gem 'inspec'
---
+++ deployment/network/infra/variables.tf
@@ -0,0 +1,9 @@
+variable "component" {}
+variable "deployment_identifier" {}
+variable "estate" {}
+variable "base_dns_domain" {}
+
+variable "aws_profile" { default = "default" }
+variable "assume_role_arn" { default = "" }
+variable "region" { default = "eu-west-1" }
+variable "availability_zones" { default = "eu-west-1a,eu-west-1b,eu-west-1c" }
