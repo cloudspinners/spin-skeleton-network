6cc95cbae3ea37e86518834f0e546adad4c6fece
kief <kmorris@thoughtworks.com>
2018-07-18 16:06:55 +0100

    Don't check in the package folder

--- package/HARDCODED-spinner/deployment/network/infra/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "${var.region}"
-  profile = "${var.aws_profile}"
-  assume_role {
-    role_arn  = "${var.assume_role_arn}"
-    session_name = "session-${var.component}-${var.estate}"
-  }
-}
